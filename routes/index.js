5806821893791d324461d4032dd93bcc43d7d8a9
dasung jung <rhakdkdl16@naver.com>
2019-12-02 11:56:18 +0900

    날짜 기능구현

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/date',function(req,res,next){
+ var date = new Date();
+ if(date != undefined)
+ {
+   var dateObj = {date: date};
+   res.status(200).send(dateObj);
+ }
+});
+
 module.exports = router;
